4855c6bad884363697ad025cf0c3bf696a540603
Adam Gausmann <adam@gaussian.dev>
2022-10-06 20:44:07 -0500

    Add based code "Hello World" example and lexer

---
+++ code/code.rs/src/main.rs
@@ -0,0 +1,211 @@
+use std::collections::HashMap;
+use std::io::{stdin, Read};
+
+fn main() {
+    let mut input = Vec::new();
+    stdin().read_to_end(&mut input).unwrap();
+    let parse = Parse::new(input.into_iter());
+    for token in parse {
+        println!("{:?}", token);
+    }
+}
+
+struct Parse<I> {
+    chars: I,
+    state: ParseState,
+}
+
+impl<I> Parse<I> {
+    fn new(chars: I) -> Self {
+        Self {
+            chars,
+            state: ParseState::Start,
+        }
+    }
+}
+
+impl<I> Iterator for Parse<I>
+where
+    I: Iterator<Item = u8>,
+{
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        loop {
+            let (next_state, token) = self.state.take().consume(self.chars.next()?);
+            self.state = next_state;
+            if let Some(token) = token {
+                return Some(token);
+            }
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum ParseState {
+    Invalid,
+    Start,
+    Pending(u8),
+    Word(Vec<u8>),
+    StringLiteral(Vec<u8>, StringLiteralState),
+}
+
+impl ParseState {
+    fn take(&mut self) -> Self {
+        std::mem::replace(self, Self::Invalid)
+    }
+
+    fn consume(self, c: u8) -> (Self, Option<Token>) {
+        match self {
+            Self::Invalid => unreachable!(),
+            Self::Start => match c {
+                b'_' | b'a'..=b'z' | b'A'..=b'Z' => (Self::Word(vec![c]), None),
+                b'"' => (
+                    Self::StringLiteral(vec![], StringLiteralState::Normal),
+                    None,
+                ),
+                b'(' => (Self::Start, Some(Token::LeftParen)),
+                b')' => (Self::Start, Some(Token::RightParen)),
+                b'{' => (Self::Start, Some(Token::LeftBrace)),
+                b'}' => (Self::Start, Some(Token::RightBrace)),
+                b':' => (Self::Start, Some(Token::Colon)),
+                b';' => (Self::Start, Some(Token::Semicolon)),
+                b'.' => (Self::Start, Some(Token::Dot)),
+                _ => (Self::Start, None),
+            },
+            Self::Pending(d) => {
+                let (pend_state, pend_token) = Self::Start.consume(d);
+                if pend_token.is_some() {
+                    assert!(pend_state == Self::Start);
+                    (Self::Pending(c), pend_token)
+                } else {
+                    pend_state.consume(c)
+                }
+            }
+            Self::Word(mut word_state) => match c {
+                b'_' | b'a'..=b'z' | b'A'..=b'Z' | b'0'..=b'9' => {
+                    word_state.push(c);
+                    (Self::Word(word_state), None)
+                }
+                _ => (
+                    Self::Pending(c),
+                    Some(match word_state.as_slice() {
+                        b"func" => Token::Keyword(Keyword::Func),
+                        _ => Token::Word(Word(word_state)),
+                    }),
+                ),
+            },
+            Self::StringLiteral(mut string, state) => match state {
+                StringLiteralState::Normal => match c {
+                    b'\\' => (
+                        Self::StringLiteral(string, StringLiteralState::Escape),
+                        None,
+                    ),
+                    b'"' => (
+                        Self::Start,
+                        Some(Token::Literal(Literal::String(StringLiteral(string)))),
+                    ),
+                    _ => {
+                        string.push(c);
+                        (Self::StringLiteral(string, state), None)
+                    }
+                },
+                StringLiteralState::Escape => match c {
+                    b'n' => {
+                        string.push(b'\n');
+                        (
+                            Self::StringLiteral(string, StringLiteralState::Normal),
+                            None,
+                        )
+                    }
+                    _ => {
+                        string.push(c);
+                        (
+                            Self::StringLiteral(string, StringLiteralState::Normal),
+                            None,
+                        )
+                    }
+                },
+            },
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum StringLiteralState {
+    Normal,
+    Escape,
+}
+
+#[derive(Debug)]
+enum Token {
+    Word(Word),
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Literal(Literal),
+    Colon,
+    Semicolon,
+    Dot,
+    Keyword(Keyword),
+}
+
+#[derive(Debug)]
+struct Word(Vec<u8>);
+
+#[derive(Debug)]
+enum Keyword {
+    Func,
+}
+
+#[derive(Debug)]
+enum Literal {
+    String(StringLiteral),
+}
+
+#[derive(Debug)]
+struct StringLiteral(Vec<u8>);
+
+struct Module {
+    functions: HashMap<String, Function>,
+}
+
+enum Item {
+    Function(Function),
+}
+
+struct Function {
+    arguments: Vec<Argument>,
+    body: Block,
+}
+
+struct Argument {
+    name: String,
+    type_: String,
+}
+
+struct Block {
+    statements: Vec<Statement>,
+}
+
+enum Statement {
+    Expression(Expression),
+}
+
+enum Expression {
+    Dot(Dot),
+    Call(Call),
+    Block(Block),
+    Literal(Literal),
+}
+
+struct Dot {
+    left: Box<Expression>,
+    right: Box<Expression>,
+}
+
+struct Call {
+    left: Box<Expression>,
+    args: Vec<Expression>,
+}
